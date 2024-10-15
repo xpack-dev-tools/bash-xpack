3b69747cc8d0d17b2c5d557f98e09cff3d53736b
Liviu Ionescu <ilg@livius.net>
2024-10-15 03:33:37 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -42,7 +42,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout 
-      title={`Welcome to bash`}
+      title="Welcome!"
       description="A binary package with the bash executables">
       <HomepageHeader />
       <main>
